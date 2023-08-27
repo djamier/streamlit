5ff526a5195570506d76e4d1bbff16517e6f3787
sastriodjamier <sastriodjamier@gmail.com>
2023-08-27 21:35:15 +0700

    add streamlit line chart and update readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ To get started, you need to activate virtual environment and install a few packa
 You can do this by invoking the following command in your terminal:
 
 ```bash
-source source config.sh
+source config.sh
 ```
 
 # How to run streamlit
---
+++ data_visualize/first_project/chart/streamlit_line_chart.py
@@ -0,0 +1,131 @@
+import streamlit as st
+import pandas as pd
+import os
+import snowflake.connector
+import plotly.express as px
+import pendulum
+
+def app():
+
+    @st.cache_resource
+    def get_snowflake_connection():
+        account = os.getenv('SNOWFLAKE_ACCOUNT_ID')
+        user = os.getenv('SNOWFLAKE_USER')
+        password = os.getenv('SNOWFLAKE_PASS')
+        role = os.getenv('SNOWFLAKE_ROLE')
+        warehouse = os.getenv('SNOWFLAKE_WAREHOUSE')
+        database = os.getenv('SNOWFLAKE_DATABASE')
+        schema = os.getenv('SNOWFLAKE_SCHEMA')
+
+        conn = snowflake.connector.connect(
+            account=account,
+            user=user,
+            password=password,
+            role=role,
+            warehouse=warehouse,
+            database=database,
+            schema=schema
+        )
+        return conn
+    
+    @st.cache_data(experimental_allow_widgets=True)
+    def get_date_range():
+        default_end_date = pendulum.today().date()
+        default_start_date = default_end_date.start_of('month')
+            
+        dates = st.date_input(
+                label='Dates', 
+                value=(default_start_date, default_end_date),
+                key='date_input'
+            )
+
+        if len(dates) != 2:
+            st.stop()
+
+        start_date, end_date = dates
+        return start_date, end_date
+
+    @st.cache_data
+    def read_query_file(
+        query_file
+    ): 
+        with open(query_file, 'r') as file:
+            query = file.read()
+        return query
+
+    @st.cache_data
+    def execute_query(
+        _conn, 
+        query :str, 
+        start_date, 
+        end_date
+    ):
+        replace_filter = {
+            'start_date_str': f"'{start_date}'",
+            'end_date_str': f"'{end_date}'"
+        }
+
+        query = query.format(**replace_filter)
+        result = pd.read_sql(
+            query,
+            _conn
+        )
+        return result
+
+    @st.cache_data
+    def create_sales_line_chart(
+        result
+    ):
+        result['ORDER_DATE'] = pd.to_datetime(
+            result['ORDER_DATE']
+        ).dt.day
+
+        fig = px.line(
+            result, 
+            x='ORDER_DATE', 
+            y='TOTAL_SALES', 
+            color='ORDER_SOURCE',
+            width=1000, 
+            color_discrete_map={
+                'Offline': '#ff9d5c',
+                'Online': '#29C5F6'
+            }
+        )
+        
+        for x, y in zip(result['ORDER_DATE'], result['TOTAL_SALES']):
+            max_yaxis = max(result['TOTAL_SALES'])
+            text = f'Rp. {y:,.0f}'
+            fig.add_annotation(
+                x=x,
+                y=y,
+                text = text,
+                showarrow=False,
+                font=dict(color='black', size=12),
+                xshift=0,
+                yshift=15
+            )
+
+            fig.update_xaxes(dtick='D1', tickformat='%Y-%m-%d')
+            fig.update_layout(
+                title='',
+                xaxis_title='Days',
+                yaxis_title='Sales',
+                yaxis_range=[0, max_yaxis],
+                xaxis_range=[0, 28],
+                xaxis=dict(
+                    tickangle=1,
+                    tickfont=dict(size=10)
+                )
+            )
+        return fig
+
+    conn = get_snowflake_connection()
+
+    start_date, end_date = get_date_range()
+    query = read_query_file('first_project/sql/sales_line_chart.sql')
+    result = execute_query(conn, query, start_date, end_date)
+    fig = create_sales_line_chart(result)
+
+    # st.header('Line chart')
+    with st.container():
+        st.plotly_chart(fig)
